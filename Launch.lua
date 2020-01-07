54f30d31d939fa7f7bda4216f402120b8edf1c9e
Tóth Ákos <akos.toth@cheppers.com>
2020-01-07 14:48:58 +0100

    remove error crash

--- Launch.lua
+++ Launch.lua
@@ -318,9 +318,6 @@ function launch:ShowPrompt(r, g, b, str, func)
 end
 
 function launch:ShowErrMsg(fmt, ...)
-	erf = io.open("C:\\Users\\akost\\Documents\\error.txt", "a")
-	erf:write(string.format(fmt, ...))
-	erf:close()
 	if not self.promptMsg then
 		self:ShowPrompt(1, 0, 0, "^1Error:\n\n^0" .. string.format(fmt, ...) .. "\n\nPress Enter/Escape to Dismiss, or F5 to restart the application.")
 	end
